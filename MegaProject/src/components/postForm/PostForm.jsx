cea8cfd06501657bc64a61a8bd1febc2a226df81
saikishore0319 <saikishor20172017@gmail.com>
2025-03-10 11:45:03 +0530

    post form finished

--- MegaProject/src/components/postForm/PostForm.jsx
+++ MegaProject/src/components/postForm/PostForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { useNavigate } from "react-router";
 import { Button, RTE, Input, Select } from "../index";
 import { useSelector } from "react-redux";
@@ -49,7 +49,90 @@ function PostForm({ post }) {
       }
     }
   };
-  return <div>PostForm</div>;
+
+  const slugTransform = useCallback((value) => {
+    if (value && typeof value === 'string')
+      return value.trim()
+        .toLowerCase()
+        .replace('', '-')
+
+    return ''
+  }, [])
+
+  useEffect(() => {
+    const subscription = watch((value, { name }) => {
+      if (name === 'title') {
+        setValue('slug', slugTransform(value.title,{shouldValidate : true}))
+      }
+    }
+    )
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [watch, slugTransform, setValue])
+
+
+  return (
+    <form onSubmit={handleSubmit(submit)} className="felx flex-wrap">
+      <div className="w-2/3 px-2">
+        <Input
+          label='Title'
+          placeHolder = "Title"
+          className="mb-4"
+          {...register("title",{
+            required : true
+          })}
+        />
+        <Input
+        label="slug"
+        placeHolder = "slug"
+        className="mb-4"
+        {...register("slug",{
+          required : true
+        })}
+        onInput={(e)=>{
+          setValue("slug",slugTransform(e.currentTarget.value),{shouldValidate : true})
+        }}
+        />
+        <RTE
+          label="content :"
+          name= "content"
+          control={control}
+          defaultValue={getValues(content)}
+        />
+      </div>
+      <div className="w-1/3 px-2 ">
+        <Input
+         type="file"
+         label="Featured image"
+         className="mb-4"
+         accept = "image/png image/jpg image/jpeg image/gif"
+         {...register("image",{required : !post})}
+        />
+        {post && (
+          <div className="w-full mb-4">
+            <img src={service.getFilePreview(post.featuredImg)}
+             alt={post.title}
+             className="rounded-lg"
+             />
+          </div>
+        )}
+        <Select
+        options={["active","inactive"]}
+        label="status"
+        className="mb-4"
+        {...register("status",{required : true})}
+        />
+        <Button
+        className="w-full"
+        type="submit"
+        bgcolour={post?"bg-green-500" : undefined}
+        >
+          {post?"Update":"Create"}
+        </Button>
+      </div>
+    </form>
+  );
 }
 
 export default PostForm;
